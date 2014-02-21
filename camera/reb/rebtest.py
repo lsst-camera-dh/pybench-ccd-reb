3e3a77ed95e01d4cb107e71058e9c61ca0571121
Laurent Le Guillou <llg@lpnhe.in2p3.fr>
2014-02-21 17:26:21 +0100

    Added high level features and a test script.

--- camera/reb/rebtest.py
+++ camera/reb/rebtest.py
@@ -15,7 +15,6 @@ R.load_program()
 # starting the clock register
 R.fpga.start_clock()
 
-
 # starting the imageClient process
 # subprocess.Popen("imageClient %d" % reb_id) -> path problem
 subprocess.Popen("imageClient %d" % reb_id, shell=True)
@@ -28,12 +27,12 @@ R.run_subroutine('clear', repeat = 10)
 
 # taking a bias
 
-time.sleep(1)
-R.run_subroutine('bias')
+# time.sleep(1)
+# R.run_subroutine('bias')
 
 # taking a frame
 
-time.sleep(15)
-R.run_subroutine('acq')
+# time.sleep(15)
+# R.run_subroutine('acq')
 
 
