e514d9a501623c9dab134cc150719bb0a292762c
Rémy Le Breton <remy.lebreton@lpnhe.in2p3.fr>
2014-06-05 15:47:59 +0200

    decoupage ok dans fonctions.py

--- tests/spots/remy/fonctions.py
+++ tests/spots/remy/fonctions.py
@@ -57,12 +57,18 @@ def fitgaussian(data):
     
     return p
 
-def CUTS(data, hauteur = 200, largeur = 200): #On coupe l'image pour ne garder que la region autour du point max
+def CUT(data, hauteur = 25, largeur = 25):
+    ''' Coupe l'image pour ne garder qu'une region autour du maximum
+    @param data: tableau de donnees a analyser
+    @param hauteur: nombre de pixels au dessus et en dessous du point max
+    @param largeur: nombre de pixels a gauche et a droite du pixel max
+
+    '''
 
     temp_max = np.where(data==np.max(data))
-    max_i = [temp_max[0][0], temp_max[1][0]]
-
-    cuts = data[y_min_cut:y_max_cut,x_min_cut:x_max_cut]
+    a, b = [temp_max[0][0], temp_max[1][0]]
+    
+    cuts = data[a - largeur:a + largeur, b - hauteur:b + hauteur]
 
     return cuts
 
--- tests/spots/remy/fonctions_test.py
+++ tests/spots/remy/fonctions_test.py
@@ -1,13 +1,19 @@
 #Pour tester les fonctions
 
 import numpy as np
+import pyfits as py
 
+def CUT(data, hauteur = 25, largeur = 25):
+    ''' Coupe l'image pour ne garder qu'une region autour du maximum
+    @param data: tableau de donnees a analyser
+    @param hauteur: nombre de pixels au dessus et en dessous du point max
+    @param largeur: nombre de pixels a gauche et a droite du pixel max
 
-def CUTS(data, hauteur = 200, largeur = 200): #On coupe l'image pour ne garder que la region autour du point max
+    '''
 
     temp_max = np.where(data==np.max(data))
-    max_i = [temp_max[0][0], temp_max[1][0]]
-
-    cuts = data[y_min_cut:y_max_cut,x_min_cut:x_max_cut]
+    a, b = [temp_max[0][0], temp_max[1][0]]
+    
+    cuts = data[a - largeur:a + largeur, b - hauteur:b + hauteur]
 
     return cuts
