f3f81ea547b27edd931ee1fdd5452e1584c6d60c
karkar <karkar@in2p3.fr>
2014-08-18 17:11:23 +0200

    add option to manage how deep the data folder is browsed for finding data files plot temp and pressure data from the shutdown

--- analysis/Sonia/BeamMap/DoAllMaps.py
+++ analysis/Sonia/BeamMap/DoAllMaps.py
@@ -9,11 +9,13 @@ else:
     dataDir = "/home/karkar/fromXPS/LSST/dataTestCCD/beamMaps/"
 
 allfiles=[]
+maxdepth = 0
 for (dirpath, dirnames, filenames) in os.walk(dataDir):
+    depth = dirpath[len(dataDir) + len(os.path.sep):].count(os.path.sep)
+    if depth == maxdepth:
+        dirnames[:] = [] # Don't recurse any deeper
     for thisfile in filenames:
         if (thisfile[-5:] == '.data'):
-            if "oldmaps" in dirpath: 
-                continue
             allfiles.append(os.path.join(dirpath,thisfile))
 if len(allfiles) == 0:
     print "Error no  .data files were found here :" , dataDir
--- analysis/Sonia/TempAndPressure/PlotTempAndPressure.py
+++ analysis/Sonia/TempAndPressure/PlotTempAndPressure.py
@@ -1,1 +1,44 @@
 __author__ = 'karkar'
+import matplotlib.pyplot as plt
+import numpy as np
+def make_patch_spines_invisible(ax):
+    ax.set_frame_on(True)
+    ax.patch.set_visible(False)
+    for sp in ax.spines.itervalues():
+        sp.set_visible(False)
+
+
+dataDir =  "/home/karkar/fromXPS/LSST/"
+Tempfilename = "temp_shutdown_12august2014.txt"
+Presfilename = "pressure_temp_shutdown_12august2014.txt"
+t = np.genfromtxt(dataDir+Tempfilename, usecols= (1,2))
+pow = np.genfromtxt(dataDir+Tempfilename, usecols= (3))
+p = np.genfromtxt(dataDir+Presfilename, usecols = (1))
+fig = plt.figure("Temperature shutdown August 12")
+fig.subplots_adjust(right=0.75)
+ax = plt.subplot(111)
+plt.grid(True)
+#print p.shape
+timeaxis = 10*(np.arange(p.shape[0]))/3600.
+#print timeaxis.shape
+p1, =ax.plot( timeaxis, p, 'red')
+ax.semilogy()
+ax.set_xlabel("Hours")
+ax.set_ylabel("mBar", color = p1.get_color())
+ax.yaxis.label.set_color(p1.get_color())
+ax.tick_params(axis='y', colors=p1.get_color())
+ax2 = ax.twinx()
+#print t.shape
+p2,p3, = ax2.plot(timeaxis[:t.shape[0]], t)
+ax2.set_ylabel("C", color = p2.get_color())
+ax2.yaxis.label.set_color(p2.get_color())
+ax2.tick_params(axis='y', colors=p2.get_color())
+#ax3 = ax.twinx()
+#ax3.spines["right"].set_position(("axes", 1.2))
+#make_patch_spines_invisible(ax3)
+#ax3.spines["right"].set_visible(True)
+#ax3.plot(range(t.shape[0]),pow, label= "heating power")
+
+#plt.show()
+fig.savefig(dataDir+"TemperatureAndPressure2014August12.png")
+
