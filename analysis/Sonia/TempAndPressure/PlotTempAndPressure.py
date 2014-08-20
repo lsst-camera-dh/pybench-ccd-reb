cd1ccb4ac0a0f3809dc87ac845ef6cdec3874eab
karkar <karkar@in2p3.fr>
2014-08-20 18:08:58 +0200

    gain estimate from data set with various exposure time ptc-style plot for data set with various laser power some plotting tools in utils

--- analysis/Sonia/TempAndPressure/PlotTempAndPressure.py
+++ analysis/Sonia/TempAndPressure/PlotTempAndPressure.py
@@ -1,18 +1,14 @@
 __author__ = 'karkar'
 import matplotlib.pyplot as plt
 import numpy as np
-def make_patch_spines_invisible(ax):
-    ax.set_frame_on(True)
-    ax.patch.set_visible(False)
-    for sp in ax.spines.itervalues():
-        sp.set_visible(False)
+#from utils import make_patch_spines_invisible
 
 
 dataDir =  "/home/karkar/fromXPS/LSST/"
 Tempfilename = "temp_shutdown_12august2014.txt"
 Presfilename = "pressure_temp_shutdown_12august2014.txt"
 t = np.genfromtxt(dataDir+Tempfilename, usecols= (1,2))
-pow = np.genfromtxt(dataDir+Tempfilename, usecols= (3))
+heat = np.genfromtxt(dataDir+Tempfilename, usecols= (3))
 p = np.genfromtxt(dataDir+Presfilename, usecols = (1))
 fig = plt.figure("Temperature shutdown August 12")
 fig.subplots_adjust(right=0.75)
@@ -37,7 +33,7 @@ ax2.tick_params(axis='y', colors=p2.get_color())
 #ax3.spines["right"].set_position(("axes", 1.2))
 #make_patch_spines_invisible(ax3)
 #ax3.spines["right"].set_visible(True)
-#ax3.plot(range(t.shape[0]),pow, label= "heating power")
+#ax3.plot(range(t.shape[0]),heat, label= "heating power")
 
 #plt.show()
 fig.savefig(dataDir+"TemperatureAndPressure2014August12.png")
