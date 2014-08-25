e93f8037b107db275b736387db3b912a4cd52862
in2p3 <you@example.com>
2014-08-25 17:34:16 +0200

    \

--- testbench/beam_map.py
+++ testbench/beam_map.py
@@ -2,11 +2,30 @@
 
 import sys
 import time
+import datetime
 
 import xmlrpclib
 
 import lsst.instruments.utils.grid as grid
 
+
+laser = xmlrpclib.ServerProxy("http://lpnlsst:8082")
+laser.connect()
+lasername = laser.checkConnection()
+if "THORLABS" not in lasername:
+    print >>sys.stderr, "No connection to the laser. Stop."
+    sys.exit(3)
+
+laserchannel = 2
+lasercurrent = 45.0
+
+
+laser.select(0)
+laser.disable()
+laser.select(laserchannel)
+laser.setCurrent(laserchannel, lasercurrent)
+laser.enable()
+laserpower = laser.getPower(laserchannel)
 
 xyz = xmlrpclib.ServerProxy("http://lpnlsst:8101/")
 
@@ -34,24 +53,35 @@ k.readContinuous(1)
 
 print "Current value ", k.getLastReading()
 
-output = open("map.data", "w")
+date = datetime.datetime.utcnow().isoformat()
 
-xyz.move({'z': 0.0})
+for z in [10.0, 30.0, 50.0, 70.0]:
 
-G = grid.Grid(xmin = 0.0, xmax = 100.0, dx = 10.0,
-              ymin = 0.0, ymax = 100.0, dy = 10.0)
+    filename = "beam-map-%s-%05.2f.data" % (date, z)
 
-for (x,y) in G:
-    # x = float(x)
-    # y = float(y)
-    print "X,Y = ", x,y
-    xyz.move({'x': x, 'y': y})
-    time.sleep(.5)
-    mesure = k.getLastReading()
-    print mesure
-    print >>output, x,y,mesure
+    output = open(filename, "w")
 
-output.close()
+    print >>output, "# BEAM MAP"
+    print >>output, "# laser channel:", laserchannel
+    print >>output, "# laser current:", lasercurrent
+    print >>output, "# laser power:", laserpower
+
+    xyz.move({'z': z})
+
+    G = grid.Grid(xmin = 0.0, xmax = 100.0, dx = 10.0,
+                  ymin = 0.0, ymax = 100.0, dy = 10.0)
+
+    for (x,y) in G:
+        print "target = ", x,y,z
+        xyz.move({'x': x, 'y': y})
+        print "real position = ", xyz.get_position()
+        time.sleep(.5)
+        mesure = k.getLastReading()
+        print mesure
+        print >>output, x, y, z, mesure
+
+    output.close()
 
 
+laser.disable()
 
