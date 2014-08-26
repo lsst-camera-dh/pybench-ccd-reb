8e8d5b2b432ad14b6b6b1036b701dec4717d8dbd
LSST <llg@lpnhe.in2p3.fr>
2014-08-26 15:28:31 +0200

    added a script to map all the Thorlabs laser diodes at once.

---
+++ testbench/beam-maps/beam-map-all.py
@@ -0,0 +1,144 @@
+#! /usr/bin/env python
+
+import sys
+import time
+import datetime
+
+import xmlrpclib
+
+import lsst.instruments.utils.grid as grid
+
+laser = xmlrpclib.ServerProxy("http://lpnlsst:8082")
+laser.connect()
+lasername = laser.checkConnection()
+if "THORLABS" not in lasername:
+    print >>sys.stderr, "No connection to the laser. Stop."
+    sys.exit(3)
+
+lasercurrents = { 1: 45.0,
+                  2: 30.0,
+                  3: 25.0,
+                  4: 30.0 }
+
+laser.select(0)
+laser.disable()
+
+xyz = xmlrpclib.ServerProxy("http://lpnlsst:8101/")
+
+print "XYZ: opening connection..."
+xyz.open()
+print "XYZ: connection established."
+
+print "XYZ: homing procedure..."
+xyz.home()
+print "XYZ at home."
+
+print "XYZ position is now ", xyz.get_position()
+
+
+# k = xmlrpclib.ServerProxy("http://lpnlsst:8087/")
+k = xmlrpclib.ServerProxy("http://lpnlsst:8102/")
+
+k.open()
+# if k.checkConnection() != '6514':
+#     print >>sys.stderr, "No connection to the photodiode Keithley 6514. Stop."
+#     sys.exit(2)
+
+if 'KEITHLEY' not in k.get_serial():
+    print >>sys.stderr, "No connection to the photodiode Keithley 6514. Stop."
+    sys.exit(2)  
+
+
+laserchannels = [1, 2, 3, 4]
+
+for laserchannel in laserchannels:
+
+    laser.select(laserchannel)
+    lasercurrent = lasercurrents[laserchannel]
+    laser.setCurrent(laserchannel, lasercurrent)
+    laser.enable()
+
+    k.send("*RST")
+    k.send("SYST:ZCH ON")
+    
+    k.send("FUNC 'CURR:DC'")
+    # k.send("CURR:RANG 2e-8")
+    k.send("CURR:RANG 2e-6")
+    k.send("SYST:ZCOR ON")
+    k.send("SYST:ZCH OFF")
+
+
+    # k.zeroCorrect()
+    # k.readContinuous(1)
+
+    # print "Current value ", k.getLastReading()
+
+    now = datetime.datetime.utcnow()
+    z = 0.0
+
+    output = open("sphere-map-z=%f-%s.data" % (z, now.isoformat()), "w")
+
+    # measure the dark current
+
+    for i in xrange(20):
+        s = k.send("READ?")
+        elts = s.split(",")
+        if len(elts) < 3:
+            print >>sys.stderr, "error: no data from the Keithley, stop."
+            sys.exit(3)
+        mesure = float(elts[0])
+        print mesure
+        print >>output, "# dark current =", measure
+        output.flush()
+        
+
+    print >>output, "# laser channel = ", laserchannel
+    print >>output, "# laser current (mA) = ", lasercurrent
+    laserpower = laser.getPower(laserchannel)
+    print >>output, "# laser power (mW) = ", laserpower
+    print >>output, "# z (mm) = ", z
+
+    xyz.move({'z': z})
+
+    G = grid.Grid(xmin = 0.0, xmax = 100.0, dx = 10.0,
+                  ymin = 0.0, ymax = 100.0, dy = 10.0)
+
+    # G = grid.Grid(xmin = 90.0, xmax = -10.0, dx = -10.0,
+    #               ymin = 0.0, ymax = 100.0, dy = 10.0)
+
+    for (x,y) in G:
+        # x = float(x)
+        # y = float(y)
+        print "X,Y = ", x,y
+        xyz.move({'x': x, 'y': y})
+        time.sleep(.5)
+        # mesure = k.getLastReading()
+        s = k.send("READ?")
+        elts = s.split(",")
+        if len(elts) < 3:
+            print >>sys.stderr, "error: no data from the Keithley, stop."
+            sys.exit(3)
+        mesure = float(elts[0])
+        print mesure
+        print >>output, x,y,mesure
+        output.flush()
+
+    # measure the dark current
+
+    for i in xrange(20):
+        s = k.send("READ?")
+        elts = s.split(",")
+        if len(elts) < 3:
+            print >>sys.stderr, "error: no data from the Keithley, stop."
+            sys.exit(3)
+        mesure = float(elts[0])
+        print mesure
+        print >>output, "# dark current =", measure
+        output.flush()
+
+
+    output.close()
+
+
+
+
