a0f36ee7d0f6c50ec933c0e2f72b0f1cfca5474f
LSST <llg@lpnhe.in2p3.fr>
2014-10-30 11:04:32 +0100

    beam stability

--- testbench/scripts/beam-maps/beam-map-all-stability.py
+++ testbench/scripts/beam-maps/beam-map-all-stability.py
@@ -54,9 +54,12 @@ if "THORLABS" not in lasername:
     print >>sys.stderr, "No connection to the laser. Stop."
     sys.exit(3)
 
-lasercurrents = { 1: 30.0,
-                  #2: 45.0,
-                  2: 60.0,
+lasercurrents = { #1: 30.0,
+                  1: 32.0,
+                  2: 45.0,
+                  #2: 47.0,
+                  #2: 59.0,
+                  #2: 60.0,
                   3: 25.0,
                   4: 30.0 }
 
@@ -88,112 +91,109 @@ if 'KEITHLEY' not in k.get_serial():
     print >>sys.stderr, "No connection to the photodiode Keithley 6514. Stop."
     sys.exit(2)  
 
-laserchannel = [nb_channel]
+laserchannel = 1
 
 print "Nb of turn in the loop : " + str(nb_turn)
-
-if option_channel = True:
     
+k.send("*RST")
+k.send("SYST:ZCH ON")
+k.send("FUNC 'CURR:DC'")
+#k.send("CURR:RANG 2e-8")
+k.send("CURR:RANG 2e-7")
+k.send("SYST:ZCOR ON")
+k.send("SYST:ZCH OFF")
+
+
+# Turn on the laser
+
+laser.select(laserchannel)
+lasercurrent = lasercurrents[laserchannel]
+laser.setCurrent(laserchannel, lasercurrent)
+laser.enable()
+print "Waiting for the laser to turn On..."
+time.sleep(7)
+
+
+
+
+# Loop
+
+for turn_in_loops in xrange(nb_turn):
+
+    print "Turn : " + str(turn_in_loops)
+
+    now = datetime.datetime.utcnow()
+    z = 0.0
+
+    output = open("sphere-map-channel=" + str(laserchannel) + "-z=%f-%s.data" % (z, now.isoformat()), "w")
+
     
-    k.send("*RST")
-    k.send("SYST:ZCH ON")
-    k.send("FUNC 'CURR:DC'")
-    # k.send("CURR:RANG 2e-8")
-    k.send("CURR:RANG 2e-7")
-    k.send("SYST:ZCOR ON")
-    k.send("SYST:ZCH OFF")
-
-
-    # Turn on the laser
-
-    laser.select(laserchannel)
-    lasercurrent = lasercurrents[laserchannel]
-    laser.setCurrent(laserchannel, lasercurrent)
-    laser.enable()
-    print "Waiting for the laser to turn On..."
-    time.sleep(7)
-
-
     print >>output, "# laser channel = ", laserchannel
     print >>output, "# laser current (mA) = ", lasercurrent
     laserpower = laser.getPower(laserchannel)
     print >>output, "# laser power (mW) = ", laserpower
     print >>output, "# z (mm) = ", z
 
-    # Loop
+    # # measure the dark current
 
-    for turn_in_loops in xrange(nb_turn):
+    # for i in xrange(20):
+    #     s = k.send("READ?")
+    #     elts = s.split(",")
+    #     if len(elts) < 3:
+    #         print >>sys.stderr, "error: no data from the Keithley, stop."
+    #         sys.exit(3)
+    #     measure = float(elts[0])
+    #     print measure
+    #     print "dark current =", measure
+    #     print >>output, "# dark current =", measure
+    #     output.flush()
 
-        print "Turn : " + str(turn_in_loops)
+    xyz.move({'z': z})
 
-        now = datetime.datetime.utcnow()
-        z = 0.0
+    G = grid.Grid(xmin = 0.0, xmax = 100.0, dx = 10.0,
+                  ymin = 0.0, ymax = 100.0, dy = 10.0)
 
-        output = open("sphere-map-channel=" + str(laserchannel) + "-z=%f-%s.data" % (z, now.isoformat()), "w")
+    # G = grid.Grid(xmin = 90.0, xmax = -10.0, dx = -10.0,
+    #               ymin = 0.0, ymax = 100.0, dy = 10.0)
 
-        # measure the dark current
-
-        for i in xrange(20):
-            s = k.send("READ?")
-            elts = s.split(",")
-            if len(elts) < 3:
-                print >>sys.stderr, "error: no data from the Keithley, stop."
-                sys.exit(3)
-            measure = float(elts[0])
-            print measure
-            print "dark current =", measure
-            print >>output, "# dark current =", measure
-            output.flush()
-
-        xyz.move({'z': z})
-
-        G = grid.Grid(xmin = 0.0, xmax = 100.0, dx = 10.0,
-                      ymin = 0.0, ymax = 100.0, dy = 10.0)
-
-        # G = grid.Grid(xmin = 90.0, xmax = -10.0, dx = -10.0,
-        #               ymin = 0.0, ymax = 100.0, dy = 10.0)
-
-        for (x,y) in G:
-            # x = float(x)
-            # y = float(y)
-            print "X,Y = ", x,y
-            xyz.move({'x': x, 'y': y})
-            time.sleep(.5)
-            # mesure = k.getLastReading()
-            s = k.send("READ?")
-            elts = s.split(",")
-            if len(elts) < 3:
-                print >>sys.stderr, "error: no data from the Keithley, stop."
-                sys.exit(3)
-            measure = float(elts[0])
-            print measure
-            print >>output, x, y, measure
-            output.flush()
-
-        # shutdown the laser
-
-        laser.disable()
-        for i in [1,2,3,4]: laser.select(-i)
-        print "Waiting for the laser to turn Off..."
-        time.sleep(2)
-
-        # measure the dark current
-
-        for i in xrange(20):
-            s = k.send("READ?")
-            elts = s.split(",")
-            if len(elts) < 3:
-                print >>sys.stderr, "error: no data from the Keithley, stop."
-                sys.exit(3)
-            measure = float(elts[0])
-            print "dark current =", measure
-            print >>output, "# dark current =", measure
-            output.flush()
+    for (x,y) in G:
+        # x = float(x)
+        # y = float(y)
+        print "X,Y = ", x,y
+        xyz.move({'x': x, 'y': y})
+        time.sleep(.5)
+        # mesure = k.getLastReading()
+        s = k.send("READ?")
+        elts = s.split(",")
+        if len(elts) < 3:
+            print >>sys.stderr, "error: no data from the Keithley, stop."
+            sys.exit(3)
+        measure = float(elts[0])
+        print measure
+        print >>output, x, y, measure
+        output.flush()
 
 
-        output.close()
+    # # measure the dark current
 
-else:
-    print "A single channel must be specified"
+    # for i in xrange(20):
+    #     s = k.send("READ?")
+    #     elts = s.split(",")
+    #     if len(elts) < 3:
+    #         print >>sys.stderr, "error: no data from the Keithley, stop."
+    #         sys.exit(3)
+    #     measure = float(elts[0])
+    #     print "dark current =", measure
+    #     print >>output, "# dark current =", measure
+    #     output.flush()
 
+
+    output.close()
+
+# shutdown the laser
+
+laser.disable()
+for i in [1,2,3,4]: laser.select(-i)
+print "Waiting for the laser to turn Off..."
+time.sleep(2)
 
