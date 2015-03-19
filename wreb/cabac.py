ab107886256fe410ff523a9064ce0d47bb48e699
Claire Juramy-Gilles <juramy@lpnhe.in2p3.fr>
2015-03-19 13:34:58 +0100

    Added multiplexer management to CABAC1, corrected readback.

--- wreb/cabac.py
+++ wreb/cabac.py
@@ -57,6 +57,11 @@ class CABAC(object):
     SPIaddress.update({"OD0EM": 8, "OD0RM": 9, "OD1EM": 10, "OD1RM": 11, "RD": 12, "GD": 13, "OG": 14, "SPA": 15,
         "P0": 0, "P1": 1, "P2": 2, "P3": 3, "S0": 4, "S1": 5, "S2": 6, "RG": 7,
         "MUX": 16, "OFMUX": 17, "EXPCLK": 18, "HIZ": 19, "SAFE": 20, "PULS": 21})
+    # Mux maps: see function for use due to complications
+    mux0map = bidi.BidiMap(["TEMP", "SPA", "OG", "GD", "RD", "OD0", "OD1"], range(1,8))
+    mux0map.update({"ATIME": 15})
+    mux1map = bidi.BidiMap(["EXT0", "EXT1"], [6, 7])
+    clockmap = bidi.BidiMap(["P0", "P1", "P2", "P3", "S0", "S1", "S2", "RG"], range(8,16))
 
     def __init__(self):
         for param in self.params:
@@ -138,15 +143,22 @@ class CABAC(object):
         If check is True, also checks against the stored value.
         """
         
-        #splits register
-        data = reg & 0xFFFF
+        #need address to find type of data
         address = (reg >> 16)  & 0x3f
 
         name = self.SPIaddress.reverse[address]
         saved = self.settings[name]
-        self.settings[name] = data
+
+        if address < self.SPIaddress["OD0EM"]:
+            value_int = parse_reg_clock(reg)[1]
+        elif address < self.SPIaddress["MUX"]:
+            value_int = parse_reg_dc(reg)
+        else:
+            value_int = reg & 0xFFFF
+        self.settings[name] = value_int
+
         if check:
-            if saved != data:
+            if saved != value_int:
                 print("Warning: unexpected value for %s: %d" % (name, data))
 
     def read_all_registers(self, regs, check=True):
@@ -175,7 +187,56 @@ class CABAC(object):
         return regs
 
 
-    #TODO: mux management
+    # ----------------------------------------------------------
+    def set_muxout(self, output0, output1=None):
+        """
+        Multiplexer management, takes in names of what we want on each mux output.
+        There are some quirks due to CABAC1 mux addressing bug.
+        Passing None, None as parameters disables both muxes.
+        :param output0: string
+        :param output1: string
+        :return:
+        """
+        ena0 = 0
+        ena1 = 0
+        add0 = 0
+        add1 = 0
+
+        if output0:
+            ena0 = 1
+            if self.mux0map.has_key(output0):
+                add0 = self.mux0map[output0]
+                if output1:
+                    if self.mux1map.has_key(output1):
+                        add1 = self.mux1map[output1]
+                        ena1 = 1
+                    else:
+                        print("Incompatible setting for multiplexers: %s, %s" % (output0, output1))
+            elif self.clockmap.has_key(output0):
+                ena1 = 1  # inverted mux blocks
+                add1 = self.clockmap[output0]
+                if output1:
+                    if self.clockmap.has_key(output1) and output1 != "RG":
+                        add0 = self.clockmap[output1]
+                    else:
+                        print("Incompatible setting for multiplexers: %s, %s" % (output0, output1))
+                        add0 = 8
+            else:
+                print("Unknown parameter for MuxOut0: %s" % output0)
+        elif output1:
+            ena1 = 1
+            if self.mux1map.has_key(output1):
+                add1 = self.mux1map[output1]
+            elif self.clockmap.has_key(output1) and output1 != "RG":
+                add0 = self.clockmap[output1]
+                ena0 = 1
+                add1 = 8
+            else:
+                print("Unknown parameter for MuxOut1: %s" % output0)
+
+        reg = ena1 << 9 + ena0 << 8 + add1 << 4 + add0
+
+        return reg
 
     # ----------------------------------------------------------
 
