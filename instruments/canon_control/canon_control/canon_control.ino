95c4f4b225c49e922d6fab14f52e6953cf9ff2c2
Rémy Le Breton <rlebret@lpnlp147.in2p3.fr>
2015-04-20 17:33:06 +0200

    reorg

--- instruments/canon_control/canon_control/canon_control.ino
+++ instruments/canon_control/canon_control/canon_control.ino
@@ -204,7 +204,7 @@ void executeCommand(char buffer[], int & index)
 
 void eraseBuffer(char buffer[], int & index)
 {
-      for(int j = 0; j < 36; j++)
+      for(int j = 0; j < 100; j++)
            {
                    buffer[j] = 0;
             }
@@ -235,7 +235,6 @@ void loop() {
   else if(state == LOW) {
     if (Serial.available() > 0){
       char incomingByte = Serial.read();
-      char valid[36] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
       switch(incomingByte){
         case 'd':
         focus_move('d');
