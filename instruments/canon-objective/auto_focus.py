a91313545cebf27e80318e28b794a8fa6fcf9852
Rémy Le Breton <remy.lebreton@lpnhe.in2p3.fr>
2015-04-30 17:58:05 +0200

    auto_focus update in canon_objective

--- instruments/canon-objective/auto_focus.py
+++ instruments/canon-objective/auto_focus.py
@@ -5,9 +5,9 @@ import glob as gl
 import Image as im
 import time
 import serial
+import pyfits as py
 
 #import lsst.instruments.dmk41au02as as d
-#import pyfits as py
 #cam = d.Camera()
 #cam.open()
 #name = "./focus_images/" + str(time.time())
@@ -27,36 +27,92 @@ import serial
 #code=input("instruction")
 #ser.write(code)
 
-images = gl.glob("./Test2_20150428/*.tif")
-images.sort()
+arduino = serial.Serial('/dev/ttyACM0', 19200, timeout=1) 
+time.sleep(5)
 
-var = []
+def launch_command(arduino, code):
+    arduino.write(code)
+    time.sleep(len(code))
+    return arduino.readlines()
+
+def compute_variance(image):
+    variance = np.var(image)
+    variance = variance/((np.max(image) + np.min(image))**2)
+    variance = np.sqrt(variance)
+    return variance
+
+def convert_steps_in_hex(s):
+    step = hex(s)[2:]
+    if (len(step) == 1) && s > 0:
+        total = "000" + str(step)
+        return total
+    elif (len(step) == 2) && s > 0:
+        total = "00" + str(step)
+        return total
+    elif (len(step) == 3) && s > 0:
+        total = "0" + str(step)
+        return total
+    elif (len(step) == 1) && s < 0:
+        total = "FFF" + str(step)
+        return total
+    elif (len(step) == 2) && s < 0:
+        total = "FF" + str(step)
+        return total
+    elif (len(step) == 3) && s < 0:
+        total = "F" + str(step)
+        return total
+
+#------------------------------------------------------
+#Command list
+
+ask_position_command = "C00000"
+end_command = "/"
+move_command = "44"
+
+#------------------------------------------------------
+cam = d.Camera()
+cam.open()
+expo = 0.033
+
+for i in arduino.readlines():
+    print i
+
+init_position = launch_command(ask_position_command + end_command)
+init_focus = cam.capture(exposure = expo)
+
+init_var = compute_variance(init_focus)
+
+init_steps = convert_steps_in_hex(50) #Init movement of 50 steps, then ask the position
+init_move = move_command + init_steps + ask_position_command + end_command
+move = init_move
 
 for i in images:
-    pic = im.open(i)
-    d = pic.getdata()
-    data = np.array(d)
-    data_min = np.min(data)
-    data_max = np.max(data)
-    
-    variance = np.var(data)
-    variance = variance/((data_max + data_min)*(data_max + data_min))
-    variance = np.sqrt(variance)
-    
-    var.append(variance)
+    new_position = launch_command(arduino, move)
 
-var = np.array(var)
-focus = images[np.argmax(var)]
+    img = cam.capture(exposure = expo)
+    img = np.array(img)
+    img_var = compute_variance(img)
 
-print focus
 
-def open_arduino():
-    ser = serial.Serial('/dev/ttyACM0', 19200, timeout=1) 
-    time.sleep(5)
-
-def launch_command():
-    code = input("Instruction ? : ")
-    ser.write(code)
-    time.sleep(len(code))
-    return ser.readlines()
-    
+#images = gl.glob("/home/rlebret/Documents/Data/Fringes/Test2_20150428/*.tif")
+#images.sort()
+#
+#var = []
+#
+#for i in images:
+#    pic = im.open(i)
+#    d = pic.getdata()
+#    data = np.array(d)
+#    data_min = np.min(data)
+#    data_max = np.max(data)
+#    
+#    variance = np.var(data)
+#    variance = variance/((data_max + data_min)*(data_max + data_min))
+#    variance = np.sqrt(variance)
+#    
+#    var.append(variance)
+#
+#var = np.array(var)
+#focus = images[np.argmax(var)]
+#
+#print focus
