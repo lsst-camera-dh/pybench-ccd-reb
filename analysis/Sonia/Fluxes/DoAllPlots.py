50e11853671460b3c5f1bf9fb2624822a4a3fde4
karkar <karkar@in2p3.fr>
2015-04-30 15:10:34 +0200

    two options : all files at once or one by one

--- analysis/Sonia/Fluxes/DoAllPlots.py
+++ analysis/Sonia/Fluxes/DoAllPlots.py
@@ -7,10 +7,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 # import FluxesPlots
 
+def doPlot( plot, ch,laserpower, prefix):
+    mytitle= plot[0]
+    print mytitle
+    imagename = mytitle.replace(" ","_")
+    imagename = prefix+"_"+imagename+".png"
+    print imagename
+    X = plot[1]
+    Y = plot[2]
+    cut = plot[3]
+    channellist = [1,2,3,4]
+    for channel in channellist:
+      cutcondition = (ch==channel) & cut
+      plt.plot(X[cutcondition], Y[cutcondition], label="{}".format(channel), marker = "*", markeredgecolor= 'None', linewidth = 0)
+    plt.grid(True)
+    # plt.xlabel("X")
+    # plt.ylabel("Y")
+    plt.title(mytitle)
+    plt.legend(title = "Laser channels ",numpoints = 1)
+    plt.savefig(imagename)
+    # fig.show()
+    plt.close()
+
+
+
+
+
 if len(sys.argv)>1:
     dataDir = sys.argv[1]
 else:
-    dataDir = "/home-local/karkar/dataLSST/metrology/2015-04-27/"
+    dataDir = "/home-local/karkar/dataLSST/metrology/2015-04-28/"
 
 # allfiles=[]
 # maxdepth = 0
@@ -25,19 +51,24 @@ else:
 #     print "Error no  .data files were found here :" , dataDir
 #
 # print "will plot from files :", allfiles
+
+allfiles= glob.glob(dataDir+"DKD-PhD-Laser-fluxes*.data")
+print "using data files: ", allfiles
+
+#version with all files data concatenated
+c = np.loadtxt(fileinput.input(allfiles,),comments='#', delimiter= ' ' , unpack=True, ndmin=0)
+
+#version  with separated analysis for each file
 # for filename in allfiles:
 #     c = np.loadtxt(filename,comments='#', delimiter= ' ' , unpack=True, ndmin=0)
 
-
-
-c = np.loadtxt(fileinput.input(glob.glob(dataDir+"*.data"),),comments='#', delimiter= ' ' , unpack=True, ndmin=0)
 print c.shape
 time = c[0]
 ch = c[1]
 lasercurrent= c[2]
 laserpower=c[3]
-DKDflux= c[4]/13
-PhDFlux = c[5]/100
+DKDflux= c[4]
+PhDFlux = c[5]
 
 
 # list of plots with plot = (title, X, Y, cut)
@@ -60,35 +91,20 @@ allPlots = [('PhDFlux VS time', time, PhDFlux, cut_time),
              ('DKDflux Over PhDFlux VS laserpower', laserpower, DKDflux/PhDFlux, cut_time),
              ]
             # cut_oneloop = ((time > 1429939335.59) & (time < 1429964839.88))
-prefix = dataDir+"/plots/"+"allData"
-# prefix = dataDir+"/plots/"+"oneLoop"
+
 plotDir = dataDir+"/plots/"
 if not os.path.isdir(plotDir):
             os.makedirs(plotDir)
 print 'Data directory is :     %s' % dataDir
 print 'Plot directory is :     %s' % plotDir
-
-def doPlot( plot, ch,laserpower, prefix):
-    mytitle= plot[0]
-    print mytitle
-    imagename = mytitle.replace(" ","_")
-    imagename = prefix+"_"+imagename+".png"
-    print imagename
-    X = plot[1]
-    Y = plot[2]
-    cut = plot[3]
-    channellist = [1,2,3,4]
-    for channel in channellist:
-      cutcondition = (ch==channel) & cut
-      plt.plot(X[cutcondition], Y[cutcondition], label="laser channel {}".format(channel), marker = "*", markeredgecolor= 'None', linewidth = 0)
-    plt.grid(True)
-    # plt.set_xlabel("X")
-    # plt.set_ylabel("Y")
-    plt.legend(title = mytitle,numpoints = 1)
-    plt.savefig(imagename)
-    plt.show()
-    plt.close()
-
+#version with all files data concatenated
+prefix = dataDir+"/plots/"+"allData"
+#version  with separated analysis for each file
+# prefix = dataDir+"/plots/"+filename[:-5].replace(dataDir,"")
+print prefix
+# prefix = dataDir+"/plots/"+"oneLoop"
 for plot in allPlots:
     print "now doing plot: ", plot[0]
     doPlot(plot,ch,laserpower, prefix )
+
+
