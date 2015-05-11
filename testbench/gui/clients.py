2cc6b74c2b1457bc26bc6d06f66b6232b7c76c10
Laurent Le Guillou <llg@lpnhe.in2p3.fr>
2015-05-11 10:46:14 +0200

    \

--- testbench/gui/clients.py
+++ testbench/gui/clients.py
@@ -24,10 +24,10 @@ screens = {
                 # 'background': 'bg-default-grey.png' },
     'www':    { 'title': "www",
                 'desktop': 5,
-                'background': 'bg-default-grey.png' },
+                'background': 'bg-ccd-grey.png' },
     'devel':  { 'title': "Development & Tests",
                 'desktop': 6,
-                'background': 'bg-default-grey.png' }
+                'background': 'bg-ccd-grey.png' }
 }
 
 
@@ -77,7 +77,49 @@ clients = {
         'commandline': 'gnome-terminal -x tail -f $HOME/logs/xyz-server.log',
         'screen': 'lights',
         'position': {'x': 1400, 'y': 30, 'w': 490.0, 'h': 300} 
-    }
+    },
     #-------------------------------------------------------------
+    # Cryo & Pumps Cryostat chamber #0 (blue)
     #
+    'cryo0': ['cryo-0'], # alias
+    'cryo-0': ['cryogeny-0', 'pump-0'],
+    # 
+    'cryogeny-0': ['lakeshore-0', 'cryo-0-temp-graph'],
+    #
+    'lakeshore-0': { 
+        'host': 'lpnlsstbench',
+        'commandline': 'lakeshore',
+        'screen': 'cryo',
+        'position': {'x': 96, 'y': 175, 'w': 400.0, 'h': 450} 
+    },
+    #
+    # gnuplot graph!'position': {'x': 96, 'y': 700, 'w': 640.0, 'h': 470} 
+    # ...
+
+    # agilent pump      1066, 800, 400, 570
+    # pfeiffer          1490, 800, 400, 570
+    # pressure gnuplot  1250, 175, 640, 470
+
+    #-------------------------------------------------------------
+    # Cryo & Pumps Cryostat chamber #1 (red)
+    # 
+    'cryo1': ['cryo-1'], # alias
+    'cryo-1': ['cryogeny-1', 'pump-1'],
+    #
+    'cryogeny-1': ['lakeshore-1', 'cryo-1-temp-graph'],
+    #
+    'lakeshore-1': { 
+        'host': 'lpnlsstbench',
+        'commandline': 'lakeshore',
+        'screen': 'cryo',
+        'position': {'x': 2016, 'y': 175, 'w': 400.0, 'h': 450} 
+    }
+    #
+    # gnuplot graph!
+
+
+    # agilent pump      2986, 800, 400, 570
+    # pfeiffer          3410, 800, 400, 570
+    # pressure gnuplot  3170, 175, 640, 470
+
 }
