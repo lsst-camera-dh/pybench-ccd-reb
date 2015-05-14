28e01243562c37d75dd5b417c3375b717f26f13e
Laurent Le Guillou <llg@lpnhe.in2p3.fr>
2015-05-15 01:00:07 +0200

    --

--- testbench/scripts/metrology/lamps_flux.py
+++ testbench/scripts/metrology/lamps_flux.py
@@ -19,19 +19,28 @@ metrology/lamps_flux
 --------------------
 
 B.qth_flux(wlrange = [300.0, 1200.0], dwl = 5.0, 
-           gratings=[0,1,2],
-           filters = [1,2,3],
+           grating = 1, filt = 1,
+           DKD_range = 2.0e-9,
+           PhD_range = 2.0e-8,
            n = 10)
 
      Will do a wavelength scanning and measure the flux obtained
      in the integral sphere and at the CCD position with the 
      calibrated DKD photodiode.
 
+B.qth_flux_all(wlrange = [300.0, 1200.0], dwl = 5.0, 
+               n = 10)
+
+     Will do a wavelength scanning for all combinations 
+     of filter and gratings.
+
 """
 
-def qth_flux(self, wlrange = [300.0, 1200.0], dwl = 5.0, 
-             gratings=[0,1,2],
-             filters = [1,2,3],
+def qth_flux(self, 
+             wlrange = [300.0, 1200.0], dwl = 5.0, 
+             grating = 1, filt = 1,
+             DKD_range = 2.0e-11,
+             PhD_range = 2.0e-7,
              n = 10):
     """
     Will do a wavelength scanning and measure the flux obtained
@@ -48,18 +57,18 @@ def qth_flux(self, wlrange = [300.0, 1200.0], dwl = 5.0,
     
     # Check if the lamp is already on
     # and the flux control also
-    # if not(self.QTH.isOn()):
-    #     self.log("Error: you should turn the lamp on first.")
-    #     return
+    if not(self.QTH.isOn()):
+        self.log("Error: you should turn the lamp on first.")
+        return
 
-    # if self.QTH.isFluxControlled():
-    #     self.log("Error: you should turn the lamp flux control on first.")
-    #     return
+    if self.QTH.isFluxControlled():
+        self.log("Error: you should turn the lamp flux control on first.")
+        return
 
     # Setup of the 2 Keithleys
     self.log("Setting up the 2 Keithleys...")
-    self.DKD.setup_current_measurements(2e-9)
-    self.PhD.setup_current_measurements(2e-8)
+    self.DKD.setup_current_measurements(DKD_range)
+    self.PhD.setup_current_measurements(PhD_range)
     self.log("Setting up the 2 Keithleys done.")
 
     # In case it is still open, close the safety shutter
@@ -86,17 +95,29 @@ def qth_flux(self, wlrange = [300.0, 1200.0], dwl = 5.0,
                             % now.isoformat()))
     f = open(datafile, "w")
 
-    for line in self.get_meta_text():
-        print >>f, "#", line
-    
     print >>f, "# fluxes on the LSST CCD testbench"
     print >>f, "# QTH lamp through monochromator"
     print >>f, "# Entrance slit = ", self.triax.getInSlit()
     print >>f, "# Exit slit = ", self.triax.getOutSlit()
     print >>f, "# time\t filter\t grating\t wavelength\t lamp current\t lamp power\t DKD flux\t Sphere flux"
 
-    # First take dark current
+    self.log("Changing to filter %d ..." % filt)
+    # Important: first close the safety shutter (as the wheel may go to unprotected positions)
+    self.ttl.closeSafetyShutter(wait=True)
+    self.ttl.moveFilter(filt)
+    self.log("Changing to filter %d done." % filt)
 
+    self.log("Changing to grating %d ..." % grating)
+    self.triax.setGrating(grating, wait=True)
+    time.sleep(2)
+    self.log("Changing to grating %d done." % grating)
+
+    # Now write meta info in the data file
+    for line in self.get_meta_text():
+        print >>f, "#", line
+
+
+    # First take dark current
     self.log("Taking dark current measurements")
     self.ttl.closeSafetyShutter(wait=True)
     self.ttl.closeShutter(wait=True)
@@ -114,45 +135,29 @@ def qth_flux(self, wlrange = [300.0, 1200.0], dwl = 5.0,
         print now, -1, -1, eff_wl, \
             lampcurrent, lamppower, dkdflux, phdflux, 0
 
-    # for ch in self.laser.allchannels:
-    for filt in filters:
-        self.log("Changing to filter %d ..." % filt)
-        # Important: first close the safety shutter
-        self.ttl.closeSafetyShutter(wait=True)
-        self.ttl.moveFilter(filt)
-        self.ttl.openSafetyShutter(wait=True)
-        self.log("Changing to filter %d done." % filt)
+    # Turn on the light
 
-        for grating in gratings:
-            self.log("Changing to grating %d ..." % grating)
-            self.triax.setGrating(grating, wait=True)
-            time.sleep(2)
-            self.log("Changing to grating %d done." % grating)
+    self.ttl.openSafetyShutter()
+    self.ttl.openShutter()
 
-            self.ttl.openShutter()
+    for wl in np.arange(wlrange[0], wlrange[1] + dwl, dwl):
+        print "set wavelength to ", wl, "nm"
+        self.triax.setWavelength(wl, wait=True)
+        time.sleep(2)
+        eff_wl = self.triax.getWavelength() # there seems to be a bug
 
-            for wl in np.arange(wlrange[0], wlrange[1] + dwl, dwl):
-                print "set wavelength to ", wl, "nm"
-                self.triax.setWavelength(wl, wait=True)
-                time.sleep(2)
-                eff_wl = self.triax.getWavelength()
+        for i in xrange(n):
+            now = time.time()
+            lampcurrent = self.QTH.getAmps()
+            lamppower = self.QTH.getWatts()
+            eff_wl = self.triax.getWavelength()
+            dkdflux = self.DKD.read_measurement()
+            phdflux = self.PhD.read_measurement()
 
-                for i in xrange(n):
-                    now = time.time()
-                    lampcurrent = self.QTH.getAmps()
-                    lamppower = self.QTH.getWatts()
-                    eff_wl = self.triax.getWavelength()
-                    dkdflux = self.DKD.read_measurement()
-                    phdflux = self.PhD.read_measurement()
-
-                    print >>f, now, filt, grating, eff_wl, \
-                        lampcurrent, lamppower, dkdflux, phdflux, 1
-                    print now, filt, grating, eff_wl, \
-                        lampcurrent, lamppower, dkdflux, phdflux, 1
-
-            self.ttl.closeShutter()
-
-    f.close()
+            print >>f, now, filt, grating, eff_wl, \
+                lampcurrent, lamppower, dkdflux, phdflux, 1
+            print now, filt, grating, eff_wl, \
+                lampcurrent, lamppower, dkdflux, phdflux, 1
 
     self.log("Taking dark current measurements")
     self.ttl.closeSafetyShutter(wait=True)
@@ -171,6 +176,8 @@ def qth_flux(self, wlrange = [300.0, 1200.0], dwl = 5.0,
         print now, filt, grating, eff_wl, \
             lampcurrent, lamppower, dkdflux, phdflux, 0
 
+    f.close()
+
     # In case...
     self.ttl.closeSafetyShutter()
     self.ttl.closeShutter()
@@ -178,5 +185,26 @@ def qth_flux(self, wlrange = [300.0, 1200.0], dwl = 5.0,
 # Attach this method to the Bench class / instance
 lsst.testbench.Bench.qth_flux = qth_flux
 
+
+
+def qth_flux_all(self,
+                 wlrange = [300.0, 1200.0], dwl = 5.0, 
+                 n = 10):
+    
+    # settings for DKD and PhD Keithleys
+
+    DKD_range = 2e-11
+    PhD_range = 2e-7 
+
+    for filt in [1,2,3]:
+        for grating in [0,1,2]:
+            self.qth_flux(wlrange = wlrange, dwl = dwl,
+                          grating = grating, filt = filt,
+                          DKD_range = DKD_range,
+                          PhD_range = PhD_range,
+                          n = n)
+
+# Attach this method to the Bench class / instance
+lsst.testbench.Bench.qth_flux_all = qth_flux_all
 
 
