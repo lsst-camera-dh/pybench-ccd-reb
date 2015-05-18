67d204ca83a22593bcb93fb8e654ab39d3a89bc2
Laurent Le Guillou <llg@lpnhe.in2p3.fr>
2015-05-18 05:48:59 +0200

    Added a function to measure the laser stability

--- testbench/scripts/metrology/laser_flux.py
+++ testbench/scripts/metrology/laser_flux.py
@@ -16,12 +16,21 @@ print """
 metrology/laser_flux_ramp
 --------------------
 
-B.laser_flux_ramp(self, channels = [1,2,3,4])
+B.laser_flux_ramp(channels = [1,2,3,4])
 
      Will do a flux ramp with each laser channels and measure 
      the flux obtained in the integral sphere and at the CCD position 
      with the calibrated DKD photodiode.
+
+B.laser_stability(channel = 2, current = 45.0, repeat=1e5)
+
+     Repeat forever the same measurement to estimate the
+     stability of the laser source.
+
+
 """
+
+#------------------------------------------------------------------------
 
 def laser_flux_ramp(self, channels = [1,2,3,4]):
     ramps = { 
@@ -104,5 +113,111 @@ def laser_flux_ramp(self, channels = [1,2,3,4]):
 
 # B.flux_ramp = types.MethodType(flux_ramp, B)
 lsst.testbench.Bench.laser_flux_ramp = laser_flux_ramp
+
+
+#------------------------------------------------------------------------
+
+def laser_stability(self, channel = 2, current = 45.0, repeat = 1e4):
+
+    # setup of the 2 Keithleys
+
+    self.DKD.setup_current_measurements(2e-9)
+    self.PhD.setup_current_measurements(2e-5)
+
+    # First turn off all the channels, in case
+    self.laser.disable()
+    for ch in self.laser.allchannels:
+        self.laser.unselect(ch)
+    time.sleep(2)
+
+    # Create the data file
+    now = datetime.datetime.utcnow()
+    datadir = os.path.join(os.getenv("HOME"),
+                            "data", "metrology",
+                            now.date().isoformat())
+
+    if not(os.path.isdir(datadir)):
+        os.makedirs(datadir)
+
+    datafile = os.path.join(datadir,
+                            ("DKD-PhD-Laser-flux-stability-%s.data"
+                             % now.isoformat()))
+    f = open(datafile, "w")
+
+    print "Enable Laser channel ", channel
+    self.laser.select(channel)
+    print "set laser current to I = ", current
+    self.laser.setCurrent(channel, current)
+    self.laser.enable()
+    time.sleep(2)
+
+    for line in self.get_meta_text():
+        print >>f, "#", line
+
+    print >>f, "# fluxes on the LSST CCD testbench"
+    print >>f, "# time\t laser channel\t laser current\t laser power\t DKD flux\t Sphere flux\t On/Off"
+
+    # Dark current, laser off, shutter off
+
+    self.log("Dark current measurement")
+    self.laser.disable()
+    self.closeShutter()
+
+    for i in xrange(10):
+        now = time.time()
+        lasercurrent = self.laser.getCurrent(channel)
+        laserpower = self.laser.getPower(channel)
+        dkdflux = self.DKD.read_measurement()
+        phdflux = self.PhD.read_measurement()
+
+        print >>f, now, channel, lasercurrent, laserpower, dkdflux, phdflux, 0
+        print now, channel, lasercurrent, laserpower, dkdflux, phdflux, 0
+
+
+    self.log("Enable the laser")
+    self.laser.enable()
+    time.sleep(2)
+    self.log("Opening the shutter")
+    self.ttl.openShutter()
+    time.sleep(2)
+        
+    for i in xrange(repeat):
+        now = time.time()
+        lasercurrent = self.laser.getCurrent(channel)
+        laserpower = self.laser.getPower(channel)
+        dkdflux = self.DKD.read_measurement()
+        phdflux = self.PhD.read_measurement()
+
+        print >>f, now, channel, lasercurrent, laserpower, dkdflux, phdflux, 1
+        print now, channel, lasercurrent, laserpower, dkdflux, phdflux, 1
+
+
+    self.log("Dark current measurement")
+    self.laser.disable()
+    self.closeShutter()
+
+    for i in xrange(10):
+        now = time.time()
+        lasercurrent = self.laser.getCurrent(channel)
+        laserpower = self.laser.getPower(channel)
+        dkdflux = self.DKD.read_measurement()
+        phdflux = self.PhD.read_measurement()
+
+        print >>f, now, channel, lasercurrent, laserpower, dkdflux, phdflux, 0
+        print now, channel, lasercurrent, laserpower, dkdflux, phdflux, 0
+
+    #
+    print "Disable Laser channel ", channel
+    self.laser.unselect(channel)
+    self.laser.disable()
+    time.sleep(2)
+
+    f.close()
+
+    self.ttl.closeShutter()
+    time.sleep(2)
+
+# B.laser_stability = types.MethodType(laser_stability, B)
+lsst.testbench.Bench.laser_stability = laser_stability
 
 
