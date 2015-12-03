f86c8cc11843693cb591c9dac4dd118d0b020f8a
Laurent Le Guillou <llg@lpnhe.in2p3.fr>
2015-12-03 17:24:09 +0100

    tests with REB3

--- testbench/config.py
+++ testbench/config.py
@@ -36,14 +36,14 @@ config = {
     # REB (version 1)
     # 
     #
-    'reb': {
-       'driver'      : 'ccd_reb',
-       'hardware'    : 'REB1',
-       'reb_id'      : 2,
-       'stripe'      : 0,
-       'xmlfile'     : 'sequencer-soi.xml',
-       'version'     : 0xb01c0008  # TODO: read it from the board
-       },
+    #'reb': {
+    #   'driver'      : 'ccd_reb',
+    #   'hardware'    : 'REB1',
+    #   'reb_id'      : 2,
+    #   'stripe'      : 0,
+    #   'xmlfile'     : 'sequencer-soi.xml',
+    #   'version'     : 0xb01c0008  # TODO: read it from the board
+    #   },
     #
     # ---------------------------------------------------------------------
     # WREB (version 1)
@@ -62,14 +62,14 @@ config = {
     # REB (version 3)
     #
     #
-    # 'reb': {
-    #     'driver'      : 'ccd_reb',
-    #     'hardware'    : 'REB3',
-    #     'reb_id'      : 0x2,
-    #     'stripe'      : 1,
-    #     'xmlfile'     : 'sequencer-wreb.xml',
-    #     'version'     : 0xb0200101
-    #     },
+     'reb': {
+         'driver'      : 'ccd_reb',
+         'hardware'    : 'REB3',
+         'reb_id'      : 0x2,
+         'stripe'      : 1,
+         'xmlfile'     : 'sequencer-wreb.xml',
+         'version'     : 0xb0200101
+         },
     # ---------------------------------------------------------------------
     # =====================================================================
     #  Image display
